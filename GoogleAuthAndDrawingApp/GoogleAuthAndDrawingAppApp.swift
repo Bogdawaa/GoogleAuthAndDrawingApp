0540d711f1b7999a448d034f070f040d378ce88a
Bogdawaa <bogdan6246@yandex.ru>
2025-05-15 18:28:29 +0300

    Initial Commit

---
+++ GoogleAuthAndDrawingApp/GoogleAuthAndDrawingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleAuthAndDrawingAppApp.swift
+//  GoogleAuthAndDrawingApp
+//
+//  Created by Bogdan Fartdinov on 15.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct GoogleAuthAndDrawingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
