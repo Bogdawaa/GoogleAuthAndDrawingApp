e9e82a3569286a542f79da60e35b87f98fe0dbd8
Bogdawaa <bogdan6246@yandex.ru>
2025-05-26 16:43:10 +0300

    refactor: widthOfString() вынесена в отдельный файл

--- GoogleAuthAndDrawingApp/Screens/MainScreen/View/DrawingView.swift
+++ GoogleAuthAndDrawingApp/Screens/MainScreen/View/DrawingView.swift
@@ -241,11 +241,3 @@ extension DrawingView {
     }
 }
 
-
-extension String {
-    func widthOfString(usingFont font: UIFont) -> CGFloat {
-        let fontAttributes = [NSAttributedString.Key.font: font]
-        let size = self.size(withAttributes: fontAttributes)
-        return size.width
-    }
-}
